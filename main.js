21991b9c0f8bb941459e9f25100237273e164de3
JavRomero89 <javieromero23@gmail.com>
2023-06-20 21:17:07 +0200

    Advance

---
+++ main.js
@@ -0,0 +1,60 @@
+const Patito = 2;
+let Osito = 8 ;
+var Tiburoncin = 10 ;
+
+
+
+
+
+
+console.log(Tiburoncin)
+console.log(Osito);
+console.log(Patito);
+
+
+
+
+
+
+
+console.log(typeof 'JavaScript') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof "") // string
+console.log(typeof false) // boolean
+console.log(typeof 3.45) // number
+console.log(typeof undefined) // undefined
+console.log(typeof NaN) // object
+
+
+
+// window.alert("Texto a mostrar");
+// window.prompt("a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let prueba = ""
+prueba = prompt("Cual es tu nombre")
+console.log(prueba);
+
+
+
+
+
+
